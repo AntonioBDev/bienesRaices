502dde39d2f25a8788f7d435cb97d7be442d1191
AntonioBDev <basi.dev9@gail.com>
2025-10-01 22:12:00 -0700

    Lectura de lista de propiedades Panel Admin

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,17 @@
 <?php
+    // Importar la BD
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    //Escribir el query
+    $query = "SELECT * FROM propiedades";
+
+    //Consultar a la BD
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    // Muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
+    // Incluye Template
     require '../includes/funciones.php';
     incluirTemplate('header');
     ?>
@@ -9,8 +21,38 @@
         <p class="alerta exito">Anuncio creado correctamente</p>
        <?php endif;?> 
       <a href="/admin/propiedades/crear.php" class="boton-verde">Nueva propiedad</a>
+
+      <!-- Mostrar tabla de propiedades -->
+       <table class="propiedades">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Titulo</th>
+              <th>Imagen</th>
+              <th>Precio</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <!-- Moatrar los resultados de la base de datos -->
+          <tbody>
+            <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)):?>
+            <tr>
+              <th><?php echo $propiedad['id'];?></th>
+              <th><?php echo $propiedad['titulo'];?></th>
+              <th><img src="/imagenes/<?php echo $propiedad['imagen'];?>" alt="" class="imagen-tabla"></th>
+              <th><?php echo $propiedad['precio'];?></th>
+              <th>
+                <a href="#" class="boton-rojo-block">Eliminar</a>
+                <a href="#" class="boton-amarillo-block">Actulizar</a>
+              </th>
+            </tr>
+            <?php endwhile; ?>
+          </tbody>
+       </table>
     </main>
   <?php
+    //cerrar la base de datos
+    mysqli_close($db);
     incluirTemplate('footer');
     ?>
    
